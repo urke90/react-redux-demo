32e38902475361fcbba058c5aadd17cdfe3b4a64
urke90 <urosbijelic90@gmail.com>
2022-04-27 11:51:56 +0200

    useDispatch hook implemented in Counter component, created 2 fns for dispatching inc/dec actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
-    // console.log('COUNTER COMPONENT');
+    console.log('COUNTER COMPONENT');
+    const dispatch = useDispatch();
 
     const counter = useSelector((state) => {
         // console.log('state', state);
         return state.counter;
     });
+
+    const incrementCounterHandler = () => dispatch({ type: 'INCREMENT' });
+    const decrementCounterHandler = () => dispatch({ type: 'DECREMENT' });
 
     const toggleCounterHandler = () => {};
 
@@ -16,6 +21,10 @@ const Counter = () => {
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
             <div className={classes.value}>{counter}</div>
+            <div>
+                <button onClick={incrementCounterHandler}>Increment</button>
+                <button onClick={decrementCounterHandler}>Decrement</button>
+            </div>
             <button onClick={toggleCounterHandler}>Toggle Counter</button>
         </main>
     );
