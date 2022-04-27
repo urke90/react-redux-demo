56750274755e200dc6b66f42d9c7cbbab601d97a
urke90 <urosbijelic90@gmail.com>
2022-04-27 15:26:47 +0200

    dispathing actions in Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,30 +1,40 @@
 import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { counterActions } from '../store/toolkit';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-    console.log('COUNTER COMPONENT');
+    // console.log('COUNTER COMPONENT');
+    // console.log('counterActions', counterActions);
     const dispatch = useDispatch();
 
     const counter = useSelector((state) => {
         // console.log('state', state);
-        return state.counter;
+        return state.counterReducer.counter;
     });
 
-    const toggleCounter = useSelector((state) => state.showCounter);
+    console.log('counter', counter);
 
-    const incrementCounterHandler = () => dispatch({ type: 'INCREMENT' });
-    const decrementCounterHandler = () => dispatch({ type: 'DECREMENT' });
+    const showCounter = useSelector(
+        (state) => state.counterReducer.showCounter
+    );
+
+    const incrementCounterHandler = () => {
+        console.log('oplio increment');
+        dispatch(counterActions.increment());
+    };
+    const decrementCounterHandler = () => dispatch(counterActions.decrement());
     const incrementByHandler = () =>
         dispatch({ type: 'INCREMENT_BY', amount: 5 });
 
-    const toggleCounterHandler = () => dispatch({ type: 'TOGGLE_COUNTER' });
+    const toggleCounterHandler = () => dispatch(counterActions.toggleCounter());
 
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
-            {toggleCounter && <div className={classes.value}>{counter}</div>}
+            {showCounter && <div className={classes.value}>{counter}</div>}
             <div>
                 <button onClick={incrementCounterHandler}>Increment</button>
                 <button onClick={decrementCounterHandler}>Decrement</button>
